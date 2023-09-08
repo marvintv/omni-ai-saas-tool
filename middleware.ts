3a45982f6d6212d7b315b82110ab24336b6c252f
Marvin <marvinvilaysack@gmail.com>
2023-09-07 21:38:53 -0700

    updated

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook", "sign-in", "sign-out"],
+  publicRoutes: ["/", "/api/webhook", "sign-in", "sign-out", "dashboard", "conversation", "settings", "api/stripe", "api/trpc"],
 });
 
 export const config = {

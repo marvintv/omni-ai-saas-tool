a9ca2efbb439d1ac36402b9dcb59ce2f5c7c7c7f
Marvin <marvinvilaysack@gmail.com>
2023-09-08 10:21:54 -0700

    update testimonials

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -3,36 +3,36 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
-  {
-    name: "Joel",
-    avatar: "J",
-    title: "Software Engineer",
-    description: "I've been using this daily!",
-  },
-  {
-    name: "Antonio",
-    avatar: "A",
-    title: "Marketer",
-    description: "I use this daily for generating new ideas. Way faster than ChatGPT!",
-  },
-  {
-    name: "Mark",
-    avatar: "M",
-    title: "Content Creator",
-    description: "This app has changed my life, cannot imagine working without it!",
-  },
-  {
-    name: "Mary",
-    avatar: "M",
-    title: "College Student",
-    description: "The best in class, I am using this every day!",
-  },
+  // {
+  //   name: "Joel",
+  //   avatar: "J",
+  //   title: "Software Engineer",
+  //   description: "I've been using this daily!",
+  // },
+  // {
+  //   name: "Antonio",
+  //   avatar: "A",
+  //   title: "Marketer",
+  //   description: "I use this daily for generating new ideas. Way faster than ChatGPT!",
+  // },
+  // {
+  //   name: "Mark",
+  //   avatar: "M",
+  //   title: "Content Creator",
+  //   description: "This app has changed my life, cannot imagine working without it!",
+  // },
+  // {
+  //   name: "Mary",
+  //   avatar: "M",
+  //   title: "College Student",
+  //   description: "The best in class, I am using this every day!",
+  // },
 ];
 
 export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
-      <h2 className="text-center text-4xl text-white font-extrabold mb-10">Testimonials</h2>
+      <h2 className="text-center text-4xl text-white font-extrabold mb-10">Start with free generations today</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {testimonials.map((item) => (
           <Card key={item.description} className="bg-[#192339] border-none text-white">
